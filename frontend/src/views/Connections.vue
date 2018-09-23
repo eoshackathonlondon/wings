e7a400a64ab258ca495890fd0229b5efb4fce2fa
Andres Berrios <andres.berrios.j@gmail.com>
2018-09-23 08:16:26 +0100

    Load connected users from the blockchain

--- frontend/src/views/Connections.vue
+++ frontend/src/views/Connections.vue
@@ -1,56 +1,56 @@
 <template lang="html">
     <div class="wrap alignLeft">
-
         <sui-table celled padded>
-        
-            <sui-table-body>
-            <sui-table-row>
+          <sui-table-body>
+            <sui-table-row v-for="user in connectedUsers" :key="user.account">
                 <sui-table-cell>
-                    <img class="profilePhotoConnected" alt="profile photo" src="../assets/valerie.png">
+                  <img class="profilePhotoConnected" alt="profile photo" src="user.profile_pic_url">
                 </sui-table-cell>
                 <sui-table-cell single-line><h3>
-                    Ashley Louise &bull; 28 &nbsp; &nbsp; &nbsp; <sui-rating icon="star" :rating="1" :max-rating="1" /> 10500
+                  {{user.name}} &bull; {{user.age}} &nbsp; &nbsp; &nbsp; <sui-rating icon="star" :rating="1" :max-rating="1" /> 10500
                 </h3></sui-table-cell>
                 <sui-table-cell>
-                Creatine supplementation is the reference compound for increasing
-                muscular creatine levels; there is variability in this increase,
-                however, with some nonresponders.
+                  Creatine supplementation is the reference compound for increasing
+                  muscular creatine levels; there is variability in this increase,
+                  however, with some nonresponders.
                 </sui-table-cell>  
             </sui-table-row>
-            <sui-table-row>     
-                <sui-table-cell>
-                    <img class="profilePhotoConnected" alt="profile photo" src="../assets/erica.png">
-                </sui-table-cell>
-                <sui-table-cell single-line><h3>
-                        Erica Caufield &bull; 23 &nbsp; &nbsp; &nbsp; <sui-rating icon="star" :rating="1" :max-rating="1" /> 900
-                </h3></sui-table-cell>
-                <sui-table-cell>
-                Creatine supplementation is the reference compound for increasing
-                muscular creatine levels; there is variability in this increase,
-                however, with some nonresponders.
-                </sui-table-cell>
-            </sui-table-row>
-            <sui-table-row>     
-            <sui-table-cell>
-                    <img class="profilePhotoConnected" alt="profile photo" src="../assets/susan.png">
-                </sui-table-cell>
-                <sui-table-cell single-line><h3>
-                        Susan Barnes &bull; 31 &nbsp; &nbsp; &nbsp; <sui-rating icon="star" :rating="1" :max-rating="1" /> 125
-                </h3></sui-table-cell>
-                <sui-table-cell>
-                Creatine supplementation is the reference compound for increasing
-                muscular creatine levels; there is variability in this increase,
-                however, with some nonresponders.
-                </sui-table-cell>
-            </sui-table-row>            
-
-            </sui-table-body>
+          </sui-table-body>
         </sui-table>
     </div>
 </template>
       
 <script>
-    export default {
-    name: 'TableRatingExample',
+import blockchainService from "../services/blockchainService.js";
+
+export default {
+  name: "TableRatingExample",
+  data() {
+    return {
+      connections: [],
+      users: []
     };
+  },
+  async mounted() {
+    this.connections = (await blockchainService.getEos().getTableRows({
+      json: true,
+      code: "wings",
+      scope: "wings",
+      table: "messages"
+    })).rows;
+    this.users = (await blockchainService.getEos().getTableRows({
+      json: true,
+      code: "wings",
+      scope: "wings",
+      table: "users"
+    })).rows;
+  },
+  computed: {
+    connectedUsers() {
+      return this.connections.map(c =>
+        this.users.find(u => u.account === c.to)
+      );
+    }
+  }
+};
 </script>
